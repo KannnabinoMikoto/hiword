fcbb908812526f88d813692e38d4a4b7454328c9
刘泽宇 <RyuYa@192.168.11.1>
2018-03-22 00:22:47 +0900

    test

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,5 +1,6 @@
 public class HiWorld {
     public  static  void  main(String[] args){
-        System.out.println("Hello github");
+        System.out.println("Hello github(update)");
+        System.out.println("Hello github1");
     }
 }
